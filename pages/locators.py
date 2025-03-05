82f820d3549a25c424f353d727f08660077b2483
LapTop <aydar.valeev@rimera.com>
2025-03-06 00:07:40 +0300

    Modify LoginPage methods

--- pages/locators.py
+++ pages/locators.py
@@ -9,7 +9,6 @@ class LoginPageLocators():
     LOGIN_PASSWORD = (By.CSS_SELECTOR, "#id_login-password")
     LOGIN_FORGOTTEN_PASSWORD = (By.CSS_SELECTOR, "#login_form a")
     LOGIN_BUTTON = (By.CSS_SELECTOR, "[name='login_submit']")
-
     REGISTER_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
     REGISTER_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password1")
     REGISTER_CONFIRM_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password2")
--- pages/login_page.py
+++ pages/login_page.py
@@ -1,4 +1,5 @@
 from .base_page import BasePage
+from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
@@ -8,13 +9,16 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
-        assert True
+        assert "login" in self.browser.current_url, "Current url doesn't contain substring"
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
-        assert True
+        assert self.is_element_present(*LoginPageLocators.LOGIN_EMAIL), "Email field in Login form isn't presented"
+        assert self.is_element_present(*LoginPageLocators.LOGIN_PASSWORD), "Password field in Login form isn't presented"
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORGOTTEN_PASSWORD), "Forgotten password link in Login form isn't presented"
+        assert self.is_element_present(*LoginPageLocators.LOGIN_BUTTON), "Submit button in Login form isn't presented"
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
-        assert True
+        assert self.is_element_present(*LoginPageLocators.REGISTER_EMAIL), "Email field in Register form isn't presented"
+        assert self.is_element_present(*LoginPageLocators.REGISTER_PASSWORD), "Password field in Register form isn't presented"
+        assert self.is_element_present(*LoginPageLocators.REGISTER_CONFIRM_PASSWORD), "Confirm password field in Register form isn't presented"
+        assert self.is_element_present(*LoginPageLocators.REGISTER_BUTTON), "Submit button in Register form isn't presented"
